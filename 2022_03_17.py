87bb7fe6bb6ef179b8581db980dfdcb17eec56c6
dmsskan0913@naver.com <dmsskan0913@naver.com>
2022-03-17 15:39:06 +0900

    first commit

---
+++ 2022_03_17.py
@@ -0,0 +1,45 @@
+print("Hello World")
+
+print("mary's cosmetics")
+
+print('신씨가 소리질렀다. "도둑이야".')
+
+print('"C:\Windows"')
+
+print("안녕하세요.\n만나서\t\t반갑습니다.")
+#\n은 줄바꿈 \t는 띄어쓰기
+
+movie_rank = ['닥터 스트레인지', '스플릿', '럭키']
+print(movie_rank)
+
+movie_rank = ['닥터 스트레인지', '스플릿', '럭키']
+movie_rank.append('배트맨')
+print(movie_rank)
+
+movie_rank = ['닥터 스트레인지', '스플릿', '럭키']
+movie_rank.append('배트맨')
+movie_rank.insert(1,'슈퍼맨')
+print(movie_rank)
+
+nums = [1, 2, 3, 4, 5]
+print(sum(nums))
+
+cook = ["피자", "김밥", "만두", "양념치킨", "족발", "피자", "김치만두", "쫄면", "소시지", "라면", "팥빙수", "김치전"]
+print(len(cook))
+
+nums = [1, 2, 3, 4, 5]
+average = sum(nums) / len(nums)
+print(average)
+
+print( 3 == 5)
+#false
+
+print( 3 < 5)
+#true
+
+x = 4
+print(1 < x < 5)
+#true
+
+print((3==3) and (4 != 3))
+#true
